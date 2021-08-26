e08206350bccf321ca4bfe4b3e6905978aeb5ac4
Gene Crumpler <genecrumpler@gmail.com>
2021-08-25 22:41:56 -0500

    test boto3 client create

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,8 @@
 import json
+import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
+    s3client = boto3.client('s3')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
