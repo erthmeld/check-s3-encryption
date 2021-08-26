2e43a4026fb95e3eadd13bb25a47a7809da2a1e8
Gene Crumpler <genecrumpler@gmail.com>
2021-08-26 17:33:23 -0500

    feat: add check encryption function

--- lambda_function.py
+++ lambda_function.py
@@ -2,23 +2,37 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    # Retrieve the list of existing buckets
-    get_buckets()
-        
-    publish_sns("testing123")
+    """Lambda function to identify unencrypted s3 buckets associated with the AWS
+    account in use and send an email notification via an SNS resource.
+    """
+    s3Client = boto3.client('s3')
+
+    all_buckets = get_buckets(s3Client)
+    unencrypted_buckets = {}
+
+    for bucket in all_buckets['Buckets']:
+        if is_bucket_encryped(s3Client, bucket['Name']):
+            print('Bucket not encrypted:  ' + bucket)
+    
+    #publish_sns("testing123", )
     
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
 
-def get_buckets():
-    s3Client = boto3.client('s3')
-    s3Buckets = s3Client.list_buckets()
-    # Output the bucket names
-    print('Existing buckets:')
-    for bucket in s3Buckets['Buckets']:
-        print(f'  {bucket["Name"]}')
+def get_buckets(client):
+    """Queries s3 via boto3 client connection to generate and return a list of 
+    s3 buckets associated with the current account/region.
+    """
+    
+    s3Buckets = client.list_buckets()
+    return s3Buckets
+
+def is_bucket_encryped(client, bucketName):
+    encryption = client.get_bucket_encryption(Bucket=bucketName)
+    print(encryption)
+    return False
         
 def publish_sns(message):
     topicARN = 'arn:aws:sns:us-east-1:996921890895:gene-crumpler-s3-encryption'
