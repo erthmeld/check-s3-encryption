27576ae22c526b2977585f5825c7a52cf3c96ef3
Gene Crumpler <genecrumpler@gmail.com>
2021-08-27 09:57:37 -0500

    refactor: Finish docstrings

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,3 @@
-import json
 import boto3
 import botocore.exceptions
 
@@ -16,9 +15,9 @@ def lambda_handler(event, context):
 class check_s3:
     """ Check s3 resources class
     
-    Class object intantiates several boto3 client connections used to 
-    interact with AWS service endpoints for the purpose of checking s3
-    resources and notifying in the event an alert needs to be sent.
+    Object of class intantiates several boto3 client connections used to 
+    interact with AWS service endpoints for the purpose of checking S3
+    resources and sending a notification via SNS in the event of an alert.
     
     Attr:
         __s3Client(object): boto3 AWS s3 client
@@ -45,12 +44,32 @@ class check_s3:
 
 
     def __set_unencrypted_buckets(self):
+        """ Private method to set the unencrypted_buckets property. """
+
         for bucket in self.__buckets['Buckets']:
             if not self.__is_bucket_encryped(bucket['Name']):
                 self.__unencryptedBuckets.append(bucket)
 
 
     def __is_bucket_encryped(self, bucketName):
+        """ Private method to check if a bucket has a server side 
+        encryption configuration 
+
+        Params:
+            bucketName(string): S3 bucket name to query
+
+        Returns:
+            bool literal of True if there is no exception caught with the 
+            error code 'ServerSideEncryptionConfigurationNotFoundError', and False 
+            otherwise.
+
+        Raises:
+            botocore ClientError exception if one is caught and it does not match 
+            the error code specified above.
+        """
+
+        # Since there is no s3 property/metadata  for encryption you must check 
+        # and catch a specific exception to verify if it's setup
         try:
             encryption = self.__s3Client.get_bucket_encryption(Bucket=bucketName)
         except botocore.exceptions.ClientError as e:
@@ -63,19 +82,37 @@ class check_s3:
 
 
     def set_unencrypted_alert_message(self, buckets, **kwargs):
+        """ Public method to set the class object's alertSubject and 
+        alertMessage properties. 
+        
+        If no kwargs params are passed in, then we use the default subject and 
+        message strings in this method. Otherwise we set the object's properties 
+        accordingly.
+
+        Params:
+            buckets(list): Names of unencrypted buckets found on S3
+        """
+
         accountId = self.__stsClient.get_caller_identity().get('Account')
+        
         if 'subject' in kwargs:
             self.alertSubject = kwargs['subject']
         else:
             self.alertSubject = "ALERT:  Unencrypted s3 buckets found"
+        
         if 'message' in kwargs:
                 self.alertSubject = kwargs['subject']
         else:
             self.alertMessage = "The s3 buckets specified below do not have a server side encryption configuration. \
-Please review and correct the configuration if required. \n\nAccount ID:  " + accountId + "\n"
+Please review and correct the configuration if required."
+
+        # Append Account ID to object's alertMessage property
+        self.alertMessage = self.alertMessage + "\n\nAccount ID:  " + accountId + ""
         
+        # Append unencrypted bucket names passed in via buckets parameter to 
+        # object's alertMessage property
         for bucket in buckets:
-            self.alertMessage = self.alertMessage + "\tBucket Name:  " + bucket['Name']
+            self.alertMessage = self.alertMessage + "\n\tBucket Name:  " + bucket['Name']
 
 
     def send_unencrypted_alerts(self, *topicList):
@@ -83,14 +120,14 @@ Please review and correct the configuration if required. \n\nAccount ID:  " + ac
         to any topics provided by *topicList param.
         
         Params:
-            *topicList(*args): List of TopicArn strings to publish to
+            *topicList(*args): Topic ARN strings to publish to
 
         Returns:
             responses(dict[TopicArn:response]): Response json data from each sns publish call
             Or a string literal describing why it did not attempt to publish
             to an SNS topic
-
         """
+
         responses = {}
         if len(self.__unencryptedBuckets) > 0:
             if len(topicList) > 0:
